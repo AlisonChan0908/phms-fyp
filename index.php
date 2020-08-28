5f0d79302a283a74d8702b6d63d60cae12e3ff6f
Alison Chan <alisonchan97@gmail.com>
2020-08-28 16:19:45 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
   if (!isset($_SESSION['username'])) {
   	$_SESSION['msg'] = "You must log in first";
-  	header('location: login.php');
+  	header('location: dashboard/production/SignupSignin/signin.php');
   }
   if (isset($_GET['logout'])) {
   	session_destroy();
