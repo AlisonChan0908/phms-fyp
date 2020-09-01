6249f937d7150d1001a81ac944a2a9ddb6a97c66
Alison Chan <alisonchan97@gmail.com>
2020-09-01 21:19:15 +0800

    Update edit_insurance.php

--- dashboard/production/edit_insurance.php
+++ dashboard/production/edit_insurance.php
@@ -37,7 +37,7 @@ header("location: insurance.php");
 
   if (!isset($_SESSION['username'])) {
   	$_SESSION['msg'] = "You must log in first";
-  	header('location: login.php');
+  	header('location: SignupSignin/signin.php');
   }
   if (isset($_GET['logout'])) {
   	session_destroy();
@@ -237,7 +237,7 @@ header("location: insurance.php");
               <a data-toggle="tooltip" data-placement="top" title="Lock">
                 <span class="glyphicon glyphicon-eye-close" aria-hidden="true"></span>
               </a>
-              <a data-toggle="tooltip" data-placement="top" title="Logout" href="login.php">
+              <a data-toggle="tooltip" data-placement="top" title="Logout" href="SignupSignin/signin.php">
                 <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
               </a>
             </div>
@@ -264,7 +264,7 @@ header("location: insurance.php");
                           <span>Settings</span>
                         </a>
                     <a class="dropdown-item"  href="javascript:;">Help</a>
-                      <a class="dropdown-item"  href="login.php"><i class="fa fa-sign-out pull-right"></i> Log Out</a>
+                      <a class="dropdown-item"  href="SignupSignin/signin.php"><i class="fa fa-sign-out pull-right"></i> Log Out</a>
                     </div>
                   </li>
   
