7ccdccf5cefc4b6d3cf8064822b167b83220bfa0
Alison Chan <alisonchan97@gmail.com>
2020-09-02 05:11:49 +0800

    Update update_emer_profile.php

--- dashboard/production/update_emer_profile.php
+++ dashboard/production/update_emer_profile.php
@@ -110,16 +110,16 @@
 
             <br />
 
-            <!-- sidebar menu -->
-            <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+                 <!-- sidebar menu -->
+                 <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
               <div class="menu_section">
                 <h3>General</h3>
                 <ul class="nav side-menu">
                   <li><a><i class="fa fa-home"></i> Home <span class="fa fa-chevron-down"></span></a>
                     <ul class="nav child_menu">
                       <li><a href="index.php">Dashboard</a></li>
-                      <li><a href="index2.php">Dashboard2</a></li>
-                      <li><a href="index3.php">Dashboard3</a></li>
+                      <li><a href="Covid-19/covid19.php">Covid-19</a></li>
+                      <li><a href="../../symptomchecker/main/index.php">Symptom Checker</a></li>
                     </ul>
                   </li>
            
