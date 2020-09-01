0c28724d3df927e799fc22f280c9bb84ed85e6fe
Alison Chan <alisonchan97@gmail.com>
2020-09-01 22:10:50 +0800

    Update measurements.php

--- dashboard/production/measurements.php
+++ dashboard/production/measurements.php
@@ -188,7 +188,7 @@
               <a data-toggle="tooltip" data-placement="top" title="Lock">
                 <span class="glyphicon glyphicon-eye-close" aria-hidden="true"></span>
               </a>
-              <a data-toggle="tooltip" data-placement="top" title="Logout" href="login.php">
+              <a data-toggle="tooltip" data-placement="top" title="Logout" href="SignupSignin/signin.php">
                 <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
               </a>
             </div>
