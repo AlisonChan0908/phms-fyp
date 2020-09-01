b82d5225a94a3dd53ddff537c1090f9e9db13995
Alison Chan <alisonchan97@gmail.com>
2020-09-01 21:19:30 +0800

    Update edit_weight.php

--- dashboard/production/edit_weight.php
+++ dashboard/production/edit_weight.php
@@ -33,7 +33,7 @@ header("location: edit_weightpage.php");
 
   if (!isset($_SESSION['username'])) {
   	$_SESSION['msg'] = "You must log in first";
-  	header('location: login.php');
+  	header('location: SignupSignin/signin.php');
   }
   if (isset($_GET['logout'])) {
   	session_destroy();
@@ -230,7 +230,7 @@ header("location: edit_weightpage.php");
               <a data-toggle="tooltip" data-placement="top" title="Lock">
                 <span class="glyphicon glyphicon-eye-close" aria-hidden="true"></span>
               </a>
-              <a data-toggle="tooltip" data-placement="top" title="Logout" href="login.php">
+              <a data-toggle="tooltip" data-placement="top" title="Logout" href="SignupSignin/signin.php">
                 <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
               </a>
             </div>
@@ -257,7 +257,7 @@ header("location: edit_weightpage.php");
                           <span>Settings</span>
                         </a>
                     <a class="dropdown-item"  href="javascript:;">Help</a>
-                      <a class="dropdown-item"  href="login.php"><i class="fa fa-sign-out pull-right"></i> Log Out</a>
+                      <a class="dropdown-item"  href="SignupSignin/signin.php"><i class="fa fa-sign-out pull-right"></i> Log Out</a>
                     </div>
                   </li>
   
