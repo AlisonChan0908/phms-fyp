aef38865f2508eca85d432015f1393afb5deacf9
Alison Chan <alisonchan97@gmail.com>
2020-09-01 21:18:19 +0800

    Update add_labresults_form.php

--- dashboard/production/add_labresults_form.php
+++ dashboard/production/add_labresults_form.php
@@ -3,7 +3,7 @@ session_start();
 
 if (!isset($_SESSION['username'])) {
   $_SESSION['msg'] = "You must log in first";
-  header('location: login.php');
+  header('location: SignupSignin/signin.php');
 }
 if (isset($_GET['logout'])) {
   session_destroy();
@@ -207,7 +207,7 @@ if (isset($_GET['logout'])) {
             <a data-toggle="tooltip" data-placement="top" title="Lock">
               <span class="glyphicon glyphicon-eye-close" aria-hidden="true"></span>
             </a>
-            <a data-toggle="tooltip" data-placement="top" title="Logout" href="login.php">
+            <a data-toggle="tooltip" data-placement="top" title="Logout" href="SignupSignin/signin.php">
               <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
             </a>
           </div>
@@ -234,7 +234,7 @@ if (isset($_GET['logout'])) {
                     <span>Settings</span>
                   </a>
                   <a class="dropdown-item" href="javascript:;">Help</a>
-                  <a class="dropdown-item" href="login.php"><i class="fa fa-sign-out pull-right"></i> Log Out</a>
+                  <a class="dropdown-item" href="SignupSignin/signin.php"><i class="fa fa-sign-out pull-right"></i> Log Out</a>
                 </div>
               </li>
 
