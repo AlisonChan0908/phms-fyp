9837cba0f878309703097c6428fb9d4f74312877
Alison Chan <alisonchan97@gmail.com>
2020-09-02 05:57:25 +0800

    Update logout.php

--- dashboard/GoogleSignIn/logout.php
+++ dashboard/GoogleSignIn/logout.php
@@ -11,6 +11,6 @@ $google_client->revokeToken();
 session_destroy();
 
 //redirect page to index.php
-header('location:index.php');
+header('location:https://phms-fyp.herokuapp.com/dashboard/production/index.php');
 
 ?>
