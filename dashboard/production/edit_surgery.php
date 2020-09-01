1d6c5687cf2743d07c6c4026cd8e457333f85f15
Alison Chan <alisonchan97@gmail.com>
2020-09-01 21:19:26 +0800

    Update edit_surgery.php

--- dashboard/production/edit_surgery.php
+++ dashboard/production/edit_surgery.php
@@ -34,7 +34,7 @@ header("location: surgery.php");
 
   if (!isset($_SESSION['username'])) {
   	$_SESSION['msg'] = "You must log in first";
-  	header('location: login.php');
+  	header('location: SignupSignin/signin.php');
   }
   if (isset($_GET['logout'])) {
   	session_destroy();
@@ -234,7 +234,7 @@ header("location: surgery.php");
               <a data-toggle="tooltip" data-placement="top" title="Lock">
                 <span class="glyphicon glyphicon-eye-close" aria-hidden="true"></span>
               </a>
-              <a data-toggle="tooltip" data-placement="top" title="Logout" href="login.php">
+              <a data-toggle="tooltip" data-placement="top" title="Logout" href="SignupSignin/signin.php">
                 <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
               </a>
             </div>
@@ -261,7 +261,7 @@ header("location: surgery.php");
                           <span>Settings</span>
                         </a>
                     <a class="dropdown-item"  href="javascript:;">Help</a>
-                      <a class="dropdown-item"  href="login.php"><i class="fa fa-sign-out pull-right"></i> Log Out</a>
+                      <a class="dropdown-item"  href="SignupSignin/signin.php"><i class="fa fa-sign-out pull-right"></i> Log Out</a>
                     </div>
                   </li>
   
