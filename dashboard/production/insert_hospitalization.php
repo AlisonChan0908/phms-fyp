9c86f91d3457c4ed1bedf9889061388386d54377
Alison Chan <alisonchan97@gmail.com>
2020-09-03 05:47:19 +0800

    Update insert_hospitalization.php

--- dashboard/production/insert_hospitalization.php
+++ dashboard/production/insert_hospitalization.php
@@ -1,3 +1,24 @@
+
+<html>
+    <head>
+    <style>
+  
+       
+.alert {
+  padding: 20px;
+  background-color: #FFFAFA;
+  color:#FA8072;
+  opacity: 1;
+  transition: opacity 0.6s;
+  margin-bottom: 15px;
+}
+
+
+
+    </style>
+    </head>
+</html>
+
 <?php
 /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
@@ -27,8 +48,11 @@ if(mysqli_num_rows($result) > 0)
     {
 //If it exists, don't insert it
 
-        echo 'Record already exists. </br> <a href="add_hospitalization_form.php">Back to add hospitalization record</a>';
-        
+      
+        echo '<div class="alert">
+                        
+<strong> Record already exists. <strong> </br></br> <a href="add_hospitalization_form.php">Back to add hospitalization record.</a>
+ </div>';
     }
 //If it does not exist, then insert it
 
