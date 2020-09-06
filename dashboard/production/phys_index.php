19b386badd2cea7c8d9bbfd96e769b084fa6dbef
Alison Chan <alisonchan97@gmail.com>
2020-09-06 08:13:28 +0800

    Update phys_index.php

--- dashboard/production/phys_index.php
+++ dashboard/production/phys_index.php
@@ -3,12 +3,12 @@
 
   if (!isset($_SESSION['username'])) {
   	$_SESSION['msg'] = "You must log in first";
-  	header('location: SignupSignin/phys_login.php');
+  	header('location: Physician/phys_login.php');
   }
   if (isset($_GET['logout'])) {
   	session_destroy();
   	unset($_SESSION['username']);
-  	header("location: SignupSignin/phys_login.php");
+  	header("location: Physician/phys_login.php");
   }
 ?>
 
