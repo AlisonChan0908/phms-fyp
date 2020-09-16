86d988e31a6dad7d9d0216fefda8c0a752049c37
Alison Chan <alisonchan97@gmail.com>
2020-09-16 13:26:19 +0800

    Update server.php

--- dashboard/production/SignupSignin/server.php
+++ dashboard/production/SignupSignin/server.php
@@ -44,7 +44,7 @@ if (isset($_POST['reg_user'])) {
 
   // Finally, register user if there are no errors in the form
   if (count($errors) == 0) {
-  	$password = $password_1;//encrypt the password before saving in the database
+  	$password = md5($password_1);//encrypt the password before saving in the database
 
   	$query = "INSERT INTO users (username, email, password) 
   			  VALUES('$username', '$email', '$password')";
@@ -68,7 +68,7 @@ if (isset($_POST['login_user'])) {
     }
   
     if (count($errors) == 0) {
-        $password = $password;
+        $password = md5($password_1);
         $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
         $results = mysqli_query($db, $query);
         $user = mysqli_fetch_assoc($results);
