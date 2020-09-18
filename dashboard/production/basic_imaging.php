a56b0138ccc4dccbe845e6413f01b2a1d0fe7c67
Alison Chan <alisonchan97@gmail.com>
2020-09-18 14:13:50 +0800

    Update basic_imaging.php

--- dashboard/production/basic_imaging.php
+++ dashboard/production/basic_imaging.php
@@ -283,7 +283,9 @@ if (isset($_GET['logout'])) {
                           $result = mysqli_query($link, $sql);
                           while($row=mysqli_fetch_array($result))
                           {
+                            $uploaded_on1= date('d/m/Y',strtotime($uploaded_on));
                             ?>
+
 
                           <tbody>
 
@@ -293,7 +295,7 @@ if (isset($_GET['logout'])) {
                               <td><?php echo $row['basic_type'] ?></td>
                               
                               <td><a href="uploads/<?php echo $row['file_name'] ?>" target="_blank">Click to view file</a></td>
-                              <td><?php echo $row['uploaded_on'] ?></td>
+                              <td><?php echo $row['uploaded_on1'] ?></td>
                                
 
                                  <!-- Delete Button -->
